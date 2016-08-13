2974f28ea4654be7ab5b4307f5fcef3abf3fb040
Cédric Belin <cedric@belin.io>
2016-08-13 14:28:45 +0200

    Updated the PHPUnit dependency

--- test/index.php
+++ test/index.php
@@ -3,5 +3,4 @@
  * Entry point of the unit tests.
  */
 $rootPath = dirname(__DIR__);
-$loader = require "$rootPath/vendor/autoload.php";
-$loader->addPsr4('phing\\', "$rootPath/lib/");
+require "$rootPath/vendor/autoload.php";
